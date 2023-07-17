ead0e961147565851c6ecc5b60828daf976785d4
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-07-17 11:23:23 +0600

    Componentized

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    
-  </div>
+  <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <NavbarComponent></NavbarComponent>
+      <hero-component></hero-component>
+      <opportunity-component></opportunity-component>
+      <invest-component></invest-component>
+      <work-component></work-component>
+      <review-component></review-component>
+
+      <future-component></future-component>
+        <FooterComponent></FooterComponent>
+      
+    </main><!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
 </template>
-<script>
-export default {
-  
-}
+<script setup>
+import FooterComponent from './section/FooterComponent.vue';
+import NavbarComponent from './section/NavbarComponent.vue'
+import HeroComponent from './section/HeroComponent.vue'
+import OpportunityComponent from './section/OpportunityComponent.vue'
+import InvestComponent from './section/InvestComponent.vue'
+import WorkComponent from './section/WorkComponent.vue'
+import ReviewComponent from './section/ReviewComponent.vue'
+import FutureComponent from './section/FutureComponent.vue'
 </script>
 <style>
-  
+  @import '../assets/css/theme.min.css';
 </style>
